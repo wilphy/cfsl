cb3e107a77b6109f057502a11bcb2118c501b003
wey <149201216@qq.com>
2019-03-20 21:59:16 +0800

    日期组件与header的重新布局

---
+++ components/epsoide/index.js
@@ -0,0 +1,34 @@
+Component({
+  properties: {
+    index: {
+      type: String,
+      observer: function(newVal, oldVal, changedPath) {
+        let val = newVal < 10 ? '0' + newVal : newVal
+        console.log(val)
+        this.setData({
+          _index: val
+        })
+      }
+    }
+  },
+
+  data: {
+    months: [
+      '一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'
+    ],
+    year: 0,
+    month: '',
+    _index: ''
+  },
+
+  attached: function() {
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth()
+
+    this.setData({
+      year: year,
+      month: this.data.months[month]
+    })
+  }
+})
