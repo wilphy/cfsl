d87e8e5ca18219c716555ce380f965a6cb58bc16
wey <149201216@qq.com>
2019-03-22 19:39:47 +0800

    加入缓存机制

--- models/classic.js
+++ models/classic.js
@@ -7,21 +7,31 @@ class ClassicModel extends HTTP {
     this.request({
       url: 'classic/latest',
       success: (res) => {
-        console.log(res)
         sCallback(res)
+        this._setLatestIndex(res.index)
+        let key = this._getKey(res.index)
+        wx.setStorageSync(key, res)
       }
     })
   }
 
   getClassic(index, nextOrPrevious, sCallback) {
-    this.request({
-      url: 'classic/' + index + '/' + nextOrPrevious,
-      success: (res) => {
-        // console.log(res)
-        sCallback(res)
-        this._setLatestIndex(res.index)
-      }
-    })
+    let key = nextOrPrevious == 'next' ? this._getKey(index + 1) : this._getKey(index - 1)
+    let classic = wx.getStorageSync(key)
+    //如果缓存中没有找到key，就向服务器发送请求
+    if (!classic) {
+      this.request({
+        url: 'classic/' + index + '/' + nextOrPrevious,
+        success: (res) => {
+          //写入缓存，下一次就可以直接从缓存中拿到classic
+          wx.setStorageSync(
+            this._getKey(res.index), res)
+          sCallback(res)
+        }
+      })
+    } else {
+      sCallback(classic)
+    }
   }
 
   isFirst(index) {
@@ -40,6 +50,11 @@ class ClassicModel extends HTTP {
   _getLatestIndex() {
     let index = wx.getStorageSync('latest')
     return index
+  }
+
+  _getKey(index) {
+    let key = 'classic-' + index
+    return key
   }
 }
 
